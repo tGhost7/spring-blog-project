eba5da4fa37d2221dc44f957e83c2d8e3e1b09c4
EternalKnight17 <eternalknight17@gmail.com>
2021-04-05 20:14:46 +0300

    initial commit

---
+++ src/main/java/com/blog/demo/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("title", "Main page");
+		return "index";
+	}
+}
